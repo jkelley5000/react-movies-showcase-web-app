d56755d3eaa1d01bb6aab968b98f0a20144b69a9
Joseph Kelley <josephkelley91@gmail.com>
2024-09-26 18:22:18 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `React Movies Showcase Web App`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
