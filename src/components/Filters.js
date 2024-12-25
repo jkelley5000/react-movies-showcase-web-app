6de6f7d126ba5abe3974140cdb332b8cb7d53644
Joseph Kelley <josephkelley91@gmail.com>
2024-12-25 15:19:24 -0500

    Defines state for the active filters. On click of one of the filters, the state of that filter is set to true/active, while the other are set to false/inactive. If a filter is set to active, add "filter selected" css class.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react"
 
 const filtersMainStyle = {
     marginTop: 5,
@@ -16,13 +16,67 @@ const filterListStyle = {
     cursor: "pointer"
 }
 
-const filterOptionsStyle = {
-
+const filterSelectedStyle = {
+    backgroundColor: "#fff",
+    color: "#000"
 }
 
+const filters = [
+    {
+        id: 1,
+        name: "allShows",
+        selected: false
+    },
+    {
+        id: 2,
+        name: "allMovies",
+        selected: false
+    },
+    {
+        id: 3,
+        name: "categories",
+        selected: false
+    },
+    {
+        id: 4,
+        name: "myList",
+        selected: false
+    }
+]
+
 const Filters = ({ onNewMoviesButtonClick }) => {
+    const [filterActive, setFilterActive] = useState({
+        allShows: false,
+        allMovies: false,
+        categories: false,
+        myList: false
+    })
+
+    const transformActiveFilter = (filter) => {
+        let transformedFilter = ""
+        if (filter.includes("-")) {
+            const index = filter.indexOf("-")
+            transformedFilter = filter.substring(0, index) + filter.charAt(index + 1).toUpperCase() + filter.substring(index + 2)
+        } else {
+            transformedFilter = filter.toLowerCase()
+        }
+        return transformedFilter
+    }
+
     const newMoviesButtonClick = (type) => {
+        onFilterSelected(type)
         onNewMoviesButtonClick(type)
+    }
+
+    const onFilterSelected = (filter) => {
+        let transformedFilter = transformActiveFilter(filter)
+        filters.map(f => {
+            if (f.name === transformedFilter) {
+                setFilterActive(active => ({ ...active, [transformedFilter]: true }))
+            } else {
+                setFilterActive(active => ({ ...active, [f.name]: false }))
+            }
+        })
     }
 
     return (
@@ -30,19 +84,29 @@ const Filters = ({ onNewMoviesButtonClick }) => {
             <div
                 style={filterListStyle}
                 onClick={() => newMoviesButtonClick('all-shows')}
+                className={`${filterActive.allShows ? 'filter-selected-style' : ""}`}
             >
                 TV Shows
             </div>
             <div
                 style={filterListStyle}
                 onClick={() => newMoviesButtonClick('all-movies')}
+                className={`${filterActive.allMovies ? 'filter-selected-style' : ""}`}
             >
                 Movies
             </div>
-            <div style={filterListStyle}>
+            <div
+                style={filterListStyle}
+                onClick={() => onFilterSelected('categories')}
+                className={`${filterActive.categories ? 'filter-selected-style' : ""}`}
+            >
                 Categories
             </div>
-            <div style={filterListStyle}>
+            <div
+                style={filterListStyle}
+                onClick={() => onFilterSelected('my-list')}
+                className={`${filterActive.myList ? 'filter-selected-style' : ""}`}
+            >
                 My List
             </div>
         </main>
